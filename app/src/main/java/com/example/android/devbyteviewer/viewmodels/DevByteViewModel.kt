9196c31610cd600eda137aac2772e7feeafb6eb7
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-19 02:10:22 +0500

    Project completed...

--- app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -17,16 +17,10 @@
 package com.example.android.devbyteviewer.viewmodels
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import com.example.android.devbyteviewer.domain.DevByteVideo
-import com.example.android.devbyteviewer.network.DevByteNetwork
-import com.example.android.devbyteviewer.network.asDomainModel
-import kotlinx.coroutines.*
+import androidx.lifecycle.*
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import kotlinx.coroutines.launch
 import java.io.IOException
 
 /**
@@ -45,24 +39,28 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * The data source this ViewModel will fetch results from.
      */
     // TODO: Add a reference to the VideosRepository class
+    private val videosRepository = VideosRepository(getDatabase(application))
 
     /**
      * A playlist of videos displayed on the screen.
      */
     // TODO: Replace the MutableLiveData and backing property below to a reference to the 'videos'
     // TODO: from the VideosRepository
+
+    val playlist = videosRepository.videos
+
     /**
      * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
      * way to set this value to observers.
      */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
+    // private val _playlist = MutableLiveData<List<DevByteVideo>>()
 
     /**
      * A playlist of videos that can be shown on the screen. Views should use this to get access
      * to the data.
      */
-    val playlist: LiveData<List<DevByteVideo>>
-        get() = _playlist
+    // val playlist: LiveData<List<DevByteVideo>>
+    //    get() = _playlist
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -95,27 +93,50 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      */
     init {
         // TODO: Replace with a call to the refreshDataFromRepository9) method
-        refreshDataFromNetwork()
+        // refreshDataFromNetwork()
+        refreshDataFromRepository()
+
     }
+
 
     /**
      * Refresh data from the repository. Use a coroutine launch to run in a
      * background thread.
      */
     // TODO: Replace with the refreshDataFromRepository() method
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = DevByteNetwork.devbytes.getPlaylist()
-            _playlist.postValue(playlist.asDomainModel())
+    private fun refreshDataFromRepository() {
 
-            _eventNetworkError.value = false
-            _isNetworkErrorShown.value = false
+        // coroutine
+        viewModelScope.launch {
 
-        } catch (networkError: IOException) {
-            // Show a Toast error message and hide the progress bar.
-            _eventNetworkError.value = true
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+
+                // Show a Toast error message and hide the progress bar.
+                if (playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+
+            }
         }
     }
+
+//    private fun refreshDataFromNetwork() = viewModelScope.launch {
+//        try {
+//            val playlist = DevByteNetwork.devbytes.getPlaylist()
+//            _playlist.postValue(playlist.asDomainModel())
+//
+//            _eventNetworkError.value = false
+//            _isNetworkErrorShown.value = false
+//
+//        } catch (networkError: IOException) {
+//            // Show a Toast error message and hide the progress bar.
+//            _eventNetworkError.value = true
+//        }
+//    }
 
     /**
      * Resets the network error flag.
